00c0fc021c26b673e08379235442ae294aab036a
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2024-03-09 11:01:15 +0100

    print the index value of 30

--- list.py
+++ list.py
@@ -15,3 +15,7 @@ my_list.pop()
 
 # Sort my_list in ascending order.
 my_list.sort()
+
+# Find and print the index of the value 30 in my_list.
+index_of_30 = my_list.index(30)
+print("Index of value 30 in my_list:", index_of_30)
