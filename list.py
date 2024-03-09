b6fd4014c2959e7f4ddd02de1a4ac9a7174ed0d9
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2024-03-09 10:58:58 +0100

    update my_list using methods

--- list.py
+++ list.py
@@ -6,3 +6,8 @@ my_list.extend([10, 20, 30, 40])
 my_list.insert(1, 15)
 # Extend my_list with another list: [50, 60, 70].
 my_list.extend([50, 60, 70])
+# Remove the last element from my_list.
+my_list.pop()
+
+# Sort my_list in ascending order.
+my_list.sort()
