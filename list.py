7e3dae63f5143615270dd930016aa903a87a2361
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2024-03-09 10:59:49 +0100

    add spacing

--- list.py
+++ list.py
@@ -1,11 +1,15 @@
 # Create an empty list called my_list.
 my_list = []
+
 # Append the following elements to my_list: 10, 20, 30, 40.
 my_list.extend([10, 20, 30, 40])
+
 # Insert the value 15 at the second position in the list.
 my_list.insert(1, 15)
+
 # Extend my_list with another list: [50, 60, 70].
 my_list.extend([50, 60, 70])
+
 # Remove the last element from my_list.
 my_list.pop()
 
