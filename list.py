e3a77db0503cf93e3334f8860e665e2be8cbe293
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2024-03-09 10:57:21 +0100

    extend my_list

--- list.py
+++ list.py
@@ -4,3 +4,5 @@ my_list = []
 my_list.extend([10, 20, 30, 40])
 # Insert the value 15 at the second position in the list.
 my_list.insert(1, 15)
+# Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
